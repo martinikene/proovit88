07203ba1f4dad24c88bf5b85f1f18ecd053c1a97
Martin Geherman <martin.geherman@gmail.com>
2023-01-16 16:45:46 +0200

    changed Grid to be more dynamic, removed Cards view and used it with the Grid layout

--- src/stories/Grid.jsx
+++ src/stories/Grid.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, {useMemo} from 'react';
+import classNames       from 'classnames/bind';
 
 import styles from './grid.module.scss';
+const cn = classNames.bind(styles);
+
+export const Cell = ({xs , sm, md, lg, xl, children}) => {
+  const generateColumnStyles = (tXs, tSm, tMd, tLg, tXl) => {
+    tXs = tXs || 4;
+    tSm = tSm || tXs;
+    tMd = tMd || tSm;
+    tLg = tLg || tMd;
+    tXl = tXl || tLg;
+
+    const newXs = tXs > 4 ? 4 : tXs;
+    const newSm = tSm > 6 ? 6 : tSm;
+    const newMd = tMd > 6 ? 6 : tMd;
+    const newLg = tLg > 12 ? 12 : tLg;
+    const newXl = tXl > 12 ? 12 : tXl;
+
+    return cn(
+      'cell',
+      {
+        [`col-xs-${newXs}`]: tXs,
+        [`col-sm-${newSm}`]: tSm,
+        [`col-md-${newMd}`]: tMd,
+        [`col-lg-${newLg}`]: tLg,
+        [`col-xl-${newXl}`]: tXl,
+      }
+    )
+  }
+  const columnStyles = useMemo(() => generateColumnStyles(xs, sm, md, lg, xl), [xs, sm, md, lg, xl] );
+  return (
+    <div className={columnStyles}>
+      {children}
+    </div>
+  )
+}
 
 export const Grid = ({children}) => {
   return (
     <div className={styles.grid}>
-        {React.Children.map(children, child => (<div className={styles.cell}>{child}</div>))}
+        {children}
     </div>
   );
 };
--- src/stories/Grid.stories.jsx
+++ src/stories/Grid.stories.jsx
@@ -1,33 +1,119 @@
 import React from 'react';
 
-import { Grid } from './Grid';
+import { Grid, Cell } from './Grid';
 
 import placeholderImage from '../../public/placeholder.jpg';
+import {Card}           from './Card';
 
 export default {
   title: 'Grid',
   component: Grid,
   parameters: {
     layout: 'fullscreen',
-  }
+    backgrounds: { default: 'Light gray' },
+  },
+  subcomponents: {
+    Card
+  },
+  argTypes: {
+    hasBackground: {
+      name: "Cells have backgrounds?"
+    },
+  },
 };
 
 const Placeholder =  () => <img src={placeholderImage}  alt={"Placeholder"} style={{width: '100%', height: 'auto'}}/>;
 
-export const grid = (args) => {
-  console.log('args: ', args);
-  const children = [];
-  for( let n=0; n < args.children_count; n++) {
-    children.push( <Placeholder />);
-  }
-
+export const grid = () => {
   return (
     <Grid>
-      {children}
+      <Cell cols={8} xs={2} sm={2} md={2} lg={3} xl={3}><Placeholder /></Cell>
+      <Cell xs={2} sm={2} md={2} lg={3} xl={3}><Placeholder /></Cell>
+      <Cell sm={2} md={2} lg={3} xl={3}><Placeholder /></Cell>
+      <Cell sm={3} md={3} lg={3} xl={3}><Placeholder /></Cell>
+      <Cell sm={3} md={3} lg={4} xl={4}><Placeholder /></Cell>
+      <Cell lg={4} xl={4}><Placeholder /></Cell>
+      <Cell lg={4} xl={4}><Placeholder /></Cell>
+      <Cell lg={6} xl={6}><Placeholder /></Cell>
+      <Cell lg={6} xl={6}><Placeholder /></Cell>
+      <Cell cols={5}><Placeholder /></Cell>
+    </Grid>
+  )
+}
+export const gridExample = () => {
+  return (
+    <Grid>
+      <Cell xs={4} sm={4} md={4} lg={8}><Placeholder /></Cell>
+      <Cell xs={2} sm={2} md={2} lg={4}><Placeholder /></Cell>
+      <Cell xs={2} sm={2} md={2} lg={4}><Placeholder /></Cell>
+      <Cell xs={2} sm={2} md={2} lg={4}><Placeholder /></Cell>
+      <Cell xs={2} sm={2} md={2} lg={4}><Placeholder /></Cell>
+      <Cell xs={2} sm={3} md={3} lg={6}><Placeholder /></Cell>
+      <Cell xs={2} sm={3} md={3} lg={6}><Placeholder /></Cell>
     </Grid>
   )
 }
 
-grid.args = {
-  children_count: 9
+const cardArgs = {
+  hasBackground: false,
+  featuredImage: placeholderImage,
+  title: "Lorem ipsum dolor",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget ligula ac nunc varius consectetur.",
+  price: 106,
+  currency: "€",
+  link: "#"
 };
+
+export const TwoItems = (args) => (
+<Grid {...args}>
+  <Cell xs={2} sm={3} md={3} lg={6}><Card {...cardArgs} hasBackground={args.hasBackground} /></Cell>
+  <Cell xs={2} sm={3} md={3} lg={6}><Card {...cardArgs} hasBackground={args.hasBackground} /></Cell>
+</Grid>
+);
+TwoItems.args = {
+  hasBackground: false
+}
+
+export const ThreeItems = (args) => (
+<Grid {...args}>
+  <Cell xs={2} lg={4}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={4}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={4}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+</Grid>
+);
+ThreeItems.args = {
+  hasBackground: false
+}
+
+export const FourItems = (args) => (
+<Grid {...args}>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+</Grid>
+);
+FourItems.args = {
+  hasBackground: false
+}
+
+export const ManyItems = (args) => (
+<Grid {...args}>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+  <Cell xs={2} lg={3}><Card {...cardArgs} hasBackground={args.hasBackground}  /></Cell>
+</Grid>
+);
+ManyItems.args = {
+  hasBackground: false
+}
